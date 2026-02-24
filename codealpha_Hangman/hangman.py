52d690f5420c7ac7fed30db217dbb4b62b655dc5
Abdulmujib Dosunmu <mujibdos4@gmail.com>
2026-02-24 17:19:01 +0100

    Refactor hangman game into functions
    Refactored hangman game logic into functions for better structure and readability.

--- codealpha_Hangman/hangman.py
+++ codealpha_Hangman/hangman.py
@@ -1,40 +1,63 @@
 import random
 
-words = ['book', 'pen', 'pencil', 'paper', 'chair']
 
-secret_word = random.choice(words)
-print("Welcome to hangman!")
+def choose_word():
+    words = ['book', 'pen', 'pencil', 'paper', 'chair']
+    return random.choice(words)
 
-display = []
-for letter in secret_word:
-    display.append('_')
 
-print(" ".join(display))
+def initialize_display(word):
+    return ['_' for _ in word]
 
 
 def guess_letter():
-    guess = input("Guess a letter: ").lower()
-    return guess
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if len(guess) == 1 and guess.isalpha():
+            return guess
+        else:
+            print("Please enter a single valid letter.")
 
 
-def check_word(guess):
-    for i in range(len(secret_word)):
-        if secret_word[i] == guess:
+def update_display(word, display, guess):
+    for i in range(len(word)):
+        if word[i] == guess:
             display[i] = guess
 
 
-lives = 6
-while "_" in display and lives > 0:
-    guess = guess_letter()
-    check_word(guess)
+def main():
+    print("Welcome to Hangman!")
+
+    secret_word = choose_word()
+    display = initialize_display(secret_word)
+    lives = 6
+    guessed_letters = []
+
     print(" ".join(display))
 
-    if guess not in secret_word:
-        lives -= 1
-        print(f"oops, wrong guess. {lives} lives left")
+    while "_" in display and lives > 0:
+        guess = guess_letter()
 
-if "_" not in display:
-    print("congratulations ! , you won ")
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
 
-else:
-    print(f"Game over!. The word was {secret_word}")
+        guessed_letters.append(guess)
+
+        if guess in secret_word:
+            update_display(secret_word, display, guess)
+            print("Correct guess!")
+        else:
+            lives -= 1
+            print(f"Wrong guess. {lives} lives remaining.")
+
+        print(" ".join(display))
+
+    if "_" not in display:
+        print("Congratulations! You won!")
+    else:
+        print(f"Game over! The word was '{secret_word}'.")
+
+
+if __name__ == "__main__":
+    main()
